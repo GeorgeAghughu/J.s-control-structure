58aa348f1b6f71adb4186303d2c4947da1c65a34
George Aghughu <georgeighodaro@yahoo.com>
2023-07-18 17:52:39 +0200

    J.s-control-strucuture-Assignment

---
+++ script.js
@@ -0,0 +1,63 @@
+let Numb = 15;
+let NumbIsDivisibleByThree = 15/3 === 5
+let NumbIsDivisibleByFive = 15/5 ===  3
+
+if ("NumbIsDivisibleByThree && NumbIsDivisibleByFive"){
+   console.log(" print ${Numb} is divisible by both 3 & 5");
+}
+ 
+else if ("NumbIsDivisibleByThree && NumbIsDivisibleByFive"){
+   console.log(" print ${Numb} is divisible by 3");
+}
+ 
+else if ("NumbIsDivisibleByFive && NumbIsDivisibleByThree"){
+   console.log(" print ${Numb} is divisible by 5");
+}
+else ("NumbIsDivisibleByThree && NumbIsDivisibleByFive") 
+   console.log ("print ${Numb} is divisible by None");
+
+
+//Q2. Using an if-else statement, determin is a year is a leap year or not.
+//Research what are the conditions to be considered a leap year.
+
+let year = 1999;
+// let year = 2000;
+// let year = 1900;
+// let year = 2024;
+
+let Year = 1460; 366; "Feb29";
+
+
+if (" leapYear is === 1460, 366, Feb29"){
+ console.log ("${1460} is every four years");
+}
+else if (" leapYear is === 366 days, Feb29"){
+ console.log ("${366} is a counts of 366 days in a year");
+}
+else if ("leapYear is === Feb29"){
+    console.log ("${Feb29} the counts of 29 in month of febraury instead of 28");
+}
+else("leapYear is not === (year)*4, (366), Feb29") 
+console.log ("${None} is a leap year" );
+
+
+//Q3. Use a loop to print all the numbers from 1 to 100 that are divisible by 3 or 4.
+
+//Add code here
+
+ for ( let i = 0; i <= 100; i++){
+ let loopDivisibleByThree = i / 3 === 0
+ let loopDivisibleByFour = i / 4 === 0
+     console.log("loop 100:", i);
+ }
+
+
+
+
+
+
+
+
+
+
+
